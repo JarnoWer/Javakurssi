707365706121c0daf8cff2cef4ac233b4c2029ca
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-01-17 21:56:11 +0200

    last save

--- Viikko1/Rikesakko.java
+++ Viikko1/Rikesakko.java
@@ -18,15 +18,15 @@ public class Rikesakko {
 			System.out.println("Liian pieni tai suuri nopeusrajoitus. Nopeusrajoitus voi olla väliltä 10-120.");
 		} else if (limit >= 10 && limit <= 60) {
 			if (speed > limit + 15) {
-				System.out.println("Rikesakko on 200 euroa.");
+				System.out.println("Rikesakko on 200");
 			} else {
-				System.out.println("Rikesakko on 170 euroa");
+				System.out.println("Rikesakko on 170");
 			}
 		} else if (limit >= 70 && limit <= 120) {
 			if (speed > limit + 15) {
-				System.out.println("Rikesakko on 200 euroa.");
+				System.out.println("Rikesakko on 200");
 			} else {
-				System.out.println("Rikesakko on 140 euroa");
+				System.out.println("Rikesakko on 140");
 			}
 		} else {
 			System.out.println("Hyvin outoa.");
