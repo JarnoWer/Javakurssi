b0843d6ac7dcf337909a901e18073144e8d0c4c8
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-01-23 07:26:27 +0200

    Alkuluku.java is working now.

--- Viikko2/Alkuluku.java
+++ Viikko2/Alkuluku.java
@@ -8,13 +8,20 @@ public class Alkuluku {
 		System.out.println("Syötä jokin positiivinen kokonaisluku: ");
 		int number = reader.nextInt();
 		reader.close();
-		int divider = 2;
-		do {
-			int remainder = number % divider;
-			if (remainder == 0) {
-				System.out.println();
+		if (number <= 1) {
+			System.out.println("Luku " + number + "on liian pieni tai negatiivinen");
+		} else if (number == 2) {
+			System.out.println("Luku" + number + " on alkuluku.");
+		} else {
+			int divider = 2;
+			while (((number % divider) != 0) && (divider <= number / 2)) {
+				divider++;
+			}
+			if (number % divider == 0) {
+				System.out.println("Luku " + number + " ei ole alkuluku.");
+			} else {
+				System.out.println("Luku " + number + " on alkuluku.");
 			}
 		}
 	}
-
 }
