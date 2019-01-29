ab4f6a0483eb9c8a0e32fb8f508f0e1279ff3f71
Wermundsen Jarno <a1704467@myy.haaga-helia.fi>
2019-01-29 12:10:22 +0200

    Hopely working week tree exercises

---
+++ src/Viikko3/KysytynLuvunIndeksi.java
@@ -0,0 +1,39 @@
+package Viikko3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class KysytynLuvunIndeksi {
+	public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        List<Integer> luvut = new ArrayList<Integer>();
+
+        System.out.println("Syötä kokonaislukuja listalle (-1 lopettaa):");
+
+        while (true) {
+            int syote = lukija.nextInt();
+
+            if (syote == -1) {
+                break;
+            } else {
+                luvut.add(syote);
+            }
+        }
+
+        System.out.println();
+
+        System.out.print("Mitä lukua etsitään? ");
+        int etsittava = lukija.nextInt();
+        lukija.close();
+        int number = 0;
+		while (number < luvut.size()) {
+			int find = 
+		}
+			if ( < nowInt) {
+				biggest = nowInt;
+			}
+			number ++;
+        
+    }
+}
