82eb4b28cd72bfca55552ee331a84c638a7d3d78
Wermundsen Jarno <a1704467@myy.haaga-helia.fi>
2019-02-05 10:55:20 +0200

    last save

--- src/Viikko4/Leikkaus.java
+++ src/Viikko4/Leikkaus.java
@@ -1,5 +1,24 @@
 package Viikko4;
 
-public class Leikkaus {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Leikkaus {
+	public static void main(String[] args) {
+		List<Integer> listA = new ArrayList<>();
+		listA.add(3);
+		listA.add(2);
+		listA.add(6);
+		listA.add(-1);
+		List<Integer> listB = new ArrayList<>();
+		listB.add(3);
+		listB.add(98);
+		listB.add(1);
+		listB.add(-1);
+		System.out.println("Lista A: " + listA);
+		System.out.println();
+	}
+	public static List<Integer> laskeLeikkaus() {
+		
+	}
 }
