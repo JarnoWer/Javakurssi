0ea02998240a850c21c693be504ff672c04020b5
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-02-16 18:22:28 +0200

    Finally week 5 exercises completed

--- src/Viikko5/YhteystietoLista.java
+++ src/Viikko5/YhteystietoLista.java
@@ -13,15 +13,15 @@ public class YhteystietoLista {
 		lista = new ArrayList<Yhteystieto>();
 
 		while (true) {
-			System.out.println("Anna seuraava komento: ");
+			System.out.print("Anna seuraava komento: ");
 			String command = reader.nextLine();
 			String[] feed = command.split(" ");
 			if (feed[0].equals("Lis‰‰")) {
 				System.out.println(putYhteystieto(feed));
 			} else if (feed[0].equals("Etsi")) {
-				System.out.println(findContact(feed));
+				System.out.println(findContact(command));
 			} else if (feed[0].equals("Poista")) {
-
+				System.out.println(removeContact(command));
 			} else if (feed[0].equals("Listaa")) {
 				System.out.println(makeList());
 			} else if (feed[0].equals("Poistu")) {
@@ -38,9 +38,11 @@ public class YhteystietoLista {
 		if (size > 3) {
 			String name = feed[1] + " " + feed[2];
 			String phone = feed[3];
+			name = name.substring(0,name.length()-1);
 			Yhteystieto contact = new Yhteystieto(name, phone);
 			addToList(contact);
-			String result = "Lis‰ttiin uusi yhteystieto ";
+			int last = lista.size() - 1;
+			String result = "Lis‰ttiin uusi yhteystieto (" + lista.get(last).toString() + ")\n";
 			return result;
 		} else {
 			return "jotain m‰t‰‰";
@@ -63,8 +65,43 @@ public class YhteystietoLista {
 		lista.add(contact);
 	}
 
-	public static String findContact(String[] feed) {
-		int size = feed.length;
-		
+	public static String findContact(String feed) {
+		// ETSI sana pois merkkijonosta
+		String wanted = feed.substring(5).toLowerCase();
+		String post = "";
+		boolean x = false;
+		int searchListLength = lista.size();
+		for (int i = 0; i < searchListLength; i++) {
+			String nowIn = lista.get(i).getNimi();
+			nowIn = nowIn.toLowerCase();
+			if (nowIn.contains(wanted)) {
+				post = lista.get(i).toString() + "\n";
+				x = true;
+			}
+		}
+		if (x == true) {
+			return post;
+		} else {
+			return "Yhteystietoa ei lˆytynyt.\n";
+		}
+	}
+
+	public static String removeContact(String feed) {
+		String name = feed.substring(7);
+		String post = "";
+		boolean x = false;
+		for (int i = 0; i < lista.size(); i++) {
+			String nowIn = lista.get(i).getNimi();
+			if (nowIn.equalsIgnoreCase(name)) {
+				lista.remove(i);
+				post = "Yhteystieto poistettiin.\n";
+				x = true;
+			}
+		}
+		if (x == true) {
+			return post;
+		} else {
+			return "Yhteystietoa ei lˆytynyt.\n";
+		}
 	}
 }
