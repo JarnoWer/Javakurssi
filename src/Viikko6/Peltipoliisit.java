586daa2cc81d7d575d39f99f18f4489a9651f667
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-02-28 21:37:34 +0200

    import java.util.Scanner;
    public class Peltipoliisit {
    	public static void main(String[] args) {
    		Scanner reader = new Scanner(System.in);
    		String highSpeeds = "";
    		System.out.print("Syötä rekisterinumerot ja nopeudet. Lopuksi kirjoita \"lopeta\":");
    		while (true) {
    			String command = reader.nextLine();
    			String[] feed = command.split(";");
    			if (feed[0].equals("lopeta")) {
    				break;
    			} else {
    				highSpeeds = highSpeeds + command + ";";
    			}
    		}
    		System.out.println("Suurin: " + getFastest(highSpeeds) + " km/h");
    		System.out.println("Pienin: " + getSlowest(highSpeeds) + " km/h");
    		System.out.println("Keskiarvo: " + getAvarage(highSpeeds) + " km/h");
    		reader.close();
    	}
    
    	private static double getAvarage(String highSpeeds) {
    		String[] results = highSpeeds.split(";");
    		int number = 1;
            int sum = 0;
    		while (number < results.length) {
    			sum = sum + Integer.parseInt(results[number]);
    			number++;
    			number++;
    		}
    
            double result = (1.0*sum)/(results.length/2);
    		return result;
    	}
    
    	private static String getSlowest(String highSpeeds) {
    		String[] results = highSpeeds.split(";");
    		int biggest = Integer.parseInt(results[1]);
    		int number = 1;
    		int plate = 0;
    		while (number < results.length) {
    			int nowInt = Integer.parseInt(results[number]);
    			if (biggest > nowInt) {
    				biggest = nowInt;
    				plate = number-1;
    			}
    			number++;
    			number++;
    		}
    		String regPlate = results[plate];
    		return regPlate + ", " + biggest;
    	}
    
    	private static String getFastest(String highSpeeds) {
    		String[] results = highSpeeds.split(";");
    		int biggest = Integer.parseInt(results[1]);
    		int number = 1;
    		int plate = 0;
    		while (number < results.length) {
    			int nowInt = Integer.parseInt(results[number]);
    			if (biggest < nowInt) {
    				biggest = nowInt;
    				plate = number-1;
    			}
    			number++;
    			number++;
    		}
    		String regPlate = results[plate];
    		return regPlate + ", " + biggest;
    	}
    Peltipoliisit works now

--- src/Viikko6/Peltipoliisit.java
+++ src/Viikko6/Peltipoliisit.java
@@ -6,8 +6,8 @@ public class Peltipoliisit {
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		String highSpeeds = "";
+		System.out.print("Syötä rekisterinumerot ja nopeudet. Lopuksi kirjoita \"lopeta\":");
 		while (true) {
-			System.out.print("Anna seuraava komento: ");
 			String command = reader.nextLine();
 			String[] feed = command.split(";");
 			if (feed[0].equals("lopeta")) {
@@ -16,25 +16,59 @@ public class Peltipoliisit {
 				highSpeeds = highSpeeds + command + ";";
 			}
 		}
-		System.out.println(highSpeeds);
-		// System.out.println("Suurin: " + getFastest(highSpeeds) + " km/h");
-		// System.out.println("Suurin: " + getSlowest(highSpeeds) + " km/h");
-		// System.out.println("Suurin: " + getAvarage(highSpeeds) + " km/h");
+		System.out.println("Suurin: " + getFastest(highSpeeds) + " km/h");
+		System.out.println("Pienin: " + getSlowest(highSpeeds) + " km/h");
+		System.out.println("Keskiarvo: " + getAvarage(highSpeeds) + " km/h");
 		reader.close();
 	}
 
-	private static String getAvarage(String[] highSpeeds) {
-		// TODO Auto-generated method stub
-		return null;
+	private static double getAvarage(String highSpeeds) {
+		String[] results = highSpeeds.split(";");
+		int number = 1;
+        int sum = 0;
+		while (number < results.length) {
+			sum = sum + Integer.parseInt(results[number]);
+			number++;
+			number++;
+		}
+
+        double result = (1.0*sum)/(results.length/2);
+		return result;
 	}
 
-	private static String getSlowest(String[] highSpeeds) {
-		// TODO Auto-generated method stub
-		return null;
+	private static String getSlowest(String highSpeeds) {
+		String[] results = highSpeeds.split(";");
+		int biggest = Integer.parseInt(results[1]);
+		int number = 1;
+		int plate = 0;
+		while (number < results.length) {
+			int nowInt = Integer.parseInt(results[number]);
+			if (biggest > nowInt) {
+				biggest = nowInt;
+				plate = number-1;
+			}
+			number++;
+			number++;
+		}
+		String regPlate = results[plate];
+		return regPlate + ", " + biggest;
 	}
 
-	private static String getFastest(String[] highSpeeds) {
-		// TODO Auto-generated method stub
-		return null;
+	private static String getFastest(String highSpeeds) {
+		String[] results = highSpeeds.split(";");
+		int biggest = Integer.parseInt(results[1]);
+		int number = 1;
+		int plate = 0;
+		while (number < results.length) {
+			int nowInt = Integer.parseInt(results[number]);
+			if (biggest < nowInt) {
+				biggest = nowInt;
+				plate = number-1;
+			}
+			number++;
+			number++;
+		}
+		String regPlate = results[plate];
+		return regPlate + ", " + biggest;
 	}
 }
