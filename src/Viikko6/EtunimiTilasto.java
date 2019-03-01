d4019572ebd94ae409ef84f09a8d86c6b26c3047
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-03-01 14:48:52 +0200

    Formal

--- src/Viikko6/EtunimiTilasto.java
+++ src/Viikko6/EtunimiTilasto.java
@@ -8,14 +8,14 @@ public class EtunimiTilasto {
 		TiedostonLukija fileReader = new TiedostonLukija("etunimet.csv");
 		List<String> rows = fileReader.lueRivit();
 		HashMap<String, Integer> nameTable = new HashMap<>();
- 		
+
 		for (String nowRow : rows) {
-		
-		String[] split = nowRow.split(";");
-		String wrongNumber = split[1];
-		wrongNumber = wrongNumber.replaceAll(" ", "");
-		int number = Integer.parseInt(wrongNumber);
-		nameTable.put(split[0], number);
+
+			String[] split = nowRow.split(";");
+			String wrongNumber = split[1];
+			wrongNumber = wrongNumber.replaceAll(" ", "");
+			int number = Integer.parseInt(wrongNumber);
+			nameTable.put(split[0], number);
+		}
 	}
-}
 }
