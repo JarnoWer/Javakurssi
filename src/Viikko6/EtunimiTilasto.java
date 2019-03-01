23ac0a1fe1d26c28b6222dd7437a4425456a0526
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-03-01 14:40:31 +0200

    Couple new exercises and EtunimiTilasto incomplete.

---
+++ src/Viikko6/EtunimiTilasto.java
@@ -0,0 +1,21 @@
+package Viikko6;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class EtunimiTilasto {
+	public static void main(String[] args) {
+		TiedostonLukija fileReader = new TiedostonLukija("etunimet.csv");
+		List<String> rows = fileReader.lueRivit();
+		HashMap<String, Integer> nameTable = new HashMap<>();
+ 		
+		for (String nowRow : rows) {
+		
+		String[] split = nowRow.split(";");
+		String wrongNumber = split[1];
+		wrongNumber = wrongNumber.replaceAll(" ", "");
+		int number = Integer.parseInt(wrongNumber);
+		nameTable.put(split[0], number);
+	}
+}
+}
---
+++ src/Viikko6/Lempinimet.java
@@ -0,0 +1,13 @@
+package Viikko6;
+
+import java.util.TreeMap;
+
+public class Lempinimet {
+	public static void main(String[] args) {
+		TreeMap<String, String> nickNames = new TreeMap<String, String>();
+		nickNames.put("Teemu", "The Finnish Flash");
+		nickNames.put("Jari", "Litti");
+		nickNames.put("Kaisa-Leena", "Kappa");
+		System.out.println(nickNames);
+	}
+}
