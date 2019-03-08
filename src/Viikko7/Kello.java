ee3661365e67d0bbd1057208b522918442b2076a
Jarno Wermundsen <jarno.wermundsen@myy.haaga-helia.fi>
2019-03-08 12:27:28 +0200

    Kello.java works now like exepted

--- src/Viikko7/Kello.java
+++ src/Viikko7/Kello.java
@@ -3,35 +3,64 @@ package Viikko7;
 public class Kello {
 	private int tunnit;
 	private int minuutit;
-	
+
 	public Kello(int hours, int minutes) {
+		if (hours >= 0 && hours < 24) {
+		} else {
+			throw new VirheellinenAikaPoikkeus("Tunnit täytyy olla välillä 0-23");
+		}
+		if (minutes >= 0 && minutes < 60) {
+		} else {
+			throw new VirheellinenAikaPoikkeus("Minuutit täytyy olla välillä 0-59");
+		}
+		this.tunnit = hours;
+		this.minuutit = minutes;
+
+	}
+
+	public void setMinuutit(int minutes) {
+		if (minutes >= 0 && minutes < 60) {
+		} else {
+			throw new VirheellinenAikaPoikkeus("Minuutit täytyy olla välillä 0-59");
+		}
+		this.minuutit = minutes;
+	}
+
+	public int getMinuutit() {
+		return this.minuutit;
+	}
+
+	public int getTunnit() {
+		return this.tunnit;
+	}
+
+	public void setTunnit(int hours) {
+		if (hours >= 0 && hours < 24) {
+		} else {
+			throw new VirheellinenAikaPoikkeus("Tunnit täytyy olla välillä 0-23");
+		}
+		this.tunnit = hours;
+	}
+
+	public void lisaaMinuutit(int minutes) {
 		try {
-			testValues(hours, minutes);
-			this.tunnit = hours;
-			this.minuutit = minutes;
+			if (minutes >= 0) {
+			} else {
+				throw new VirheellinenAikaPoikkeus("Minuutteja voi vain lisätä");
+			}
+			minuutit = minuutit + minutes;
+			while (minuutit >= 60) {
+				minuutit = minuutit - 60;
+				tunnit++;
+				if (tunnit >= 24) {
+					tunnit = 0;
+				}
+			}
 		} catch (VirheellinenAikaPoikkeus e) {
 			System.out.println(e.getMessage());
 		}
-			
-		}
-	private void testValues(int hours, int minutes) {
-		if (hours > 0 && hours < 24) {
-			
-		} else {
-			throw new VirheellinenAikaPoikkeus("Virhe syötteessä");
-		}
 	}
-	public void lisaaMinuutit(int minutes) {
-		
-		minuutit = minuutit + minutes;
-		while (minuutit >= 60) {
-			minuutit = minuutit -60;
-			tunnit ++;
-			if (tunnit >= 24) {
-				tunnit = 0;
-			}
-		}
-	}
+
 	@Override
 	public String toString() {
 		return tunnit + ":" + String.format("%02d", minuutit);
--- src/Viikko7/KelloTesti.java
+++ src/Viikko7/KelloTesti.java
@@ -3,8 +3,18 @@ package Viikko7;
 public class KelloTesti {
 
 	public static void main(String[] args) {
-		Kello kello = new Kello(28, 12);
+		Kello kello = new Kello(11, 22);
 		System.out.println(kello);
+		kello.lisaaMinuutit(-29);
+		System.out.println(kello);
+		kello.lisaaMinuutit(135);
+		System.out.println(kello);
+		kello.setMinuutit(15);
+		System.out.println(kello);
+		kello.setMinuutit(61);
+		System.out.println(kello.getTunnit());
+		Kello kello2 = new Kello(11, -5);
+		System.out.println(kello2);
 	}
 
 }
